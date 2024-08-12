69abfdd00efa373f035ce62d34973e1ddfb2144b
MaximilianAichner <maximilian.aichner@crystal-design.com>
2024-08-12 14:57:12 +0200

    Delete local directory

--- local/script/copytext.js
+++
@@ -1,9 +0,0 @@
-function copyText(elementId) {
-    // Get the text from the element
-    var copyText = document.getElementById(elementId).innerText;
-
-    // Copy the text to the clipboard
-    navigator.clipboard.writeText(copyText).catch(function(err) {
-        console.error('Async: Could not copy text: ', err);
-    });
-}
