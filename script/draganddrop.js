3ccb4537c7b094364364a89322e46809baf2b5fa
MaximilianAichner <maximilian.aichner@crystal-design.com>
2024-08-19 15:27:07 +0200

    Update draganddrop.js

--- script/draganddrop.js
+++ script/draganddrop.js
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const formData = new FormData();
     formData.append("audio", file);
 
-    fetch("http://localhost:5000/transcribe", {
+    fetch("/transcribe", {
       method: "POST",
       body: formData,
     })
